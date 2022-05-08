f0686385b0d6e9092e1d4fdcf249592d162ab674
Kim-Yun-Seo <rlagrace410@gmail.com>
2022-05-08 12:34:23 +0900

    코딩 1차 테스트기 완성

--- usedcar_dealing_refactoring.js
+++ usedcar_dealing_refactoring.js
@@ -1,62 +1,73 @@
 'use strict';
 
-
 function solution(prices) {
   let maxNumber = 0;
-
-  let max = Math.max(...prices)
-  let min = Math.min(...prices)
-  let maxIndex = prices.lastIndexOf(max); 
-  let minIndex = prices.indexOf(min) 
+  const max = Math.max(...prices)
+  const min = Math.min(...prices)
+  const maxIndex = prices.lastIndexOf(max); 
+  const minIndex = prices.indexOf(min) 
 
   if (minIndex < maxIndex) {
     maxNumber = max - min;
+    console.log('여기 탔음 = ');
+  } else {
+    prices.forEach((today, todayIndex) => {
+      for (let i = todayIndex + 1; i < prices.length; i++) {
+        const minus = prices[i] - today
+        if (minus > maxNumber) {
+          maxNumber = minus
+        }
+      }
+    });
   }
   console.log(' maxNumber = ', maxNumber);
-
-  // prices.forEach((today, todayIndex) => {
-  //   for (let i = todayIndex + 1; i < prices.length; i++) {
-  //     const minus = prices[i] - today
-  //     if (minus > maxNumber) {
-  //       maxNumber = minus
-  //     }
-  //   }
-  //   // prices.forEach((future, futureIndex) => {
-  //   //   console.log('forEach', futureIndex, ' = ', future);
-  //   //   if ((futureIndex > todayIndex) && (future - today) > maxNumber) {
-  //   //     maxNumber = future - today
-  //   //   }
-  //   // });
-  // });
 
   return maxNumber;
 };
 
-console.log('solution() = ', solution([3, 5, 1, 5, 1, 4, 10000]));
-// console.log('solution = ', solution([5, 4, 3, 2, 1]))
-
 function solution2(prices) {
-  
-  let minusResult = prices.map((priceNum, priceNumIndex) => {
-    prices.forEach((num, numIndex) => {
-      console.log('priceNum = ', priceNum);
-      console.log(' num = ', num);
-      let minus = 0;
-      if (priceNumIndex < numIndex) {
-        minus = num - priceNum
-      } else {
-        minus = 0;
-      }
+  let maxNumber = 0;
 
-      console.log('minus = ', minus);
-      console.log(' ----------- ' );
-      
-      return [];
-    })
-    
-  })
-  
-  return ;
+  prices.forEach((today, todayIndex) => {
+    for (let i = todayIndex + 1; i < prices.length; i++) {
+      const minus = prices[i] - today
+      if (minus > maxNumber) {
+        maxNumber = minus
+      }
+    }
+  });
+  return maxNumber;
 };
 
-// console.log('solution2 = ', solution2([5, 4, 3, 2, 0]))
+const getRandomInt = (min, max) =>  Math.floor(Math.random() * (max - min) + min); 
+const test = (count) => {
+  const condition = 10000;
+  const maxLimit = 100000;
+
+  for (let k = 0; k < count; k++) {
+    console.log('------------ = ');
+    const testPrices = [];
+    for (let i = 0; i < condition; i++) {
+      testPrices.push(getRandomInt(0, maxLimit));
+    }
+    const answerStartTime = performance.now()
+    const aaa = solution(testPrices);
+    const answerEndTime = performance.now()
+    const totalAnswerTime = answerEndTime - answerStartTime
+    console.log(' totalAnswerTime = ', totalAnswerTime);
+
+    const studentStartTime = performance.now()
+    const bbb = solution2(testPrices);
+    const studentEndTime = performance.now()
+    const totalStudentTime = studentEndTime - studentStartTime
+    console.log(' totalStudentTime = ', totalStudentTime);
+    
+    if (aaa === bbb) {
+      console.log('예상 답은' ,aaa, '그리고 당신의 답은', bbb, '정답  = ');
+    } else {
+      console.log('예상 답은' ,aaa, '그리고 당신의 답은', bbb, '오답  = ');
+    }
+  }
+}
+test(10)
+
