559a9b84be947aadf0ea018d5a12a77346f17f6f
Kim-Yun-Seo <rlagrace410@gmail.com>
2022-05-14 17:15:33 +0900

    숫자문자열과영단어 최종완성

--- 숫자문자열과영단어.js
+++ 숫자문자열과영단어.js
@@ -1,37 +1,21 @@
 'use strict';
-const numberEnglish = [
-  {number: 0, English: 'zero'},
-  {number: 1, English: 'one'},
-  {number: 2, English: 'two'},
-  {number: 3, English: 'three'},
-  {number: 4, English: 'four'},
-  {number: 5, English: 'five'},
-  {number: 6, English: 'six'},
-  {number: 7, English: 'seven'},
-  {number: 8, English: 'eight'},
-  {number: 9, English: 'nine'}
-]
+
 
 function solution(question) {
+  const numberEnglish  = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
   let changeNumber = question;
-  numberEnglish.forEach((numberObject, numberIndex) => {
-    if (question.includes(numberObject.English) && numberIndex === 0) {
-      changeNumber = question.replace(`${numberObject.English}`,`${numberObject.number}`)
-    } else if (question.includes(numberObject.English) ) {
-        for (let i = 0; i < 10; i++) {
-          if (question.includes(numberObject.English)) {
-            changeNumber = changeNumber.replace(`${numberObject.English}`,`${numberObject.number}`)
-          }
-        }
-      } 
-    })
-  let result = Number(changeNumber)
-  return result;
-}
 
+  numberEnglish.forEach((number, index) => {
+    let regExp = new RegExp(number, 'g');
+    changeNumber = changeNumber.replace(regExp, index);
+    
+  });
+  return Number(changeNumber);
+}
 
 console.log('solution = ', solution("one4seveneight"));
 console.log('solution = ', solution("23four5six7"));
-console.log('solution = ', solution("2three45sixsix"));
+console.log('solution = ', solution("one2131231239oneoneoneoneone"));
+console.log('solution = ', solution("oneoneoneoneoneoneoneoneoneoneoneoneoneoneoneone "));
 console.log('solution = ', solution("123"));
 console.log('solution = ', solution("oneoneoneone"));
